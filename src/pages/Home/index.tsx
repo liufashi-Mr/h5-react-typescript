94b6e396b52c54d73c8863dbee4d44ffea1b6636
liufashi <liufashi@yuantutech.com>
2022-05-06 15:16:57 +0800

    feat: nav

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import styles from "./index.module.scss";
 import Layout from "@components/Layout";
 const Home: React.FC<Record<string, never>> = () => {
-  return <Layout className={styles.test}>home</Layout>;
+  return (
+    <Layout>
+      <div className={styles.test}>home</div>
+    </Layout>
+  );
 };
 export default Home;
